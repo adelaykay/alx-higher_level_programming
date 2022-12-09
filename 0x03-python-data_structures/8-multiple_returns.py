208ef0232dafce7e0dfac11b1edc5bfda40e4aef
adelaykay <adelekeolasope@gmail.com>
2022-12-09 14:00:19 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 
 def multiple_returns(sentence):
-    l = len(sentence)
-    f = sentence[0] if (l != 0) else (None)
-    return(l, f)
+    length = len(sentence)
+    first = sentence[0] if (length != 0) else (None)
+    return(length, first)
