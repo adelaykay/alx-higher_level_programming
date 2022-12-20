902bd16f3495415e3ae2e760eb073a715811e308
adelaykay <adelekeolasope@gmail.com>
2022-12-20 05:20:52 +0100

    tasks 2 and 3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         while i < x:
-            if type(my_list[i]) == type(3):
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
             i += 1
